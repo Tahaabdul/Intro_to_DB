2a793390c6e1166e95db4596e43ed8554274d2d7
Tahaabdul <rhaxaqi@gmail.com>
2024-10-20 22:46:21 +0000

    correction Task 4

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,13 @@
 -- Use the database specified as an argument
 USE alx_book_store;
 
--- Show columns of the Books table
-SHOW COLUMNS FROM Books;
+-- Query to get the full description of the Books table
+SELECT 
+    TABLE_NAME
+    COLUMN_NAME, 
+    COLUMN_TYPE 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
